cc66d380cfd335dce6f3c088d5749cb9483a7787
Sebastien Beauvais <sbeauvais7@gmail.com>
2023-11-26 17:51:23 -0800

    Adding StorePortal

---
+++ StoreProject/StorePortal/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreBusinessLayer.LogicServices;
+using StoreBusinessObjectLayer.DataBaseEntities;
+
+namespace StorePortal.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly IProductsLogic _productsDataAccess;
+
+		// constructor for controller
+		public ProductController(IProductsLogic productsDataAccess)
+		{
+			_productsDataAccess = productsDataAccess;
+		}
+
+		[HttpGet]
+		public IActionResult ProductsList()
+		{
+            List<StoreBusinessObjectLayer.DataBaseEntities.Products> result = new List<StoreBusinessObjectLayer.DataBaseEntities.Products>();
+
+			// getting products from the business layer 
+			result = _productsDataAccess.GetProductsListLogic().ToList();
+
+			return View();
+		}
+	}
+}
+
